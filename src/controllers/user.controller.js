d75cd5f0c13eee5ea057e7d17d070f9ae427863a
Sunil <sunil.world8988@gmail.com>
2025-01-08 16:17:16 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,62 @@
 import asyncHandler from '../utils/asyncHandler.js';
+import ApiError from '../utils/apiError.js';
+import { User } from '../models/user.model.js';
+import { uploadOnCloudinary } from '../utils/cloudinary.js';
+import { ApiResponse } from '../utils/apiResponse.js';
 
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Register User' });
+
+    const { fullName, email, username, password } = req.body;
+    console.log("email", email);
+
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existedUser = User.findOne({ $or: [{ username }, { email }] });
+    if (existedUser) {
+        throw new ApiError(400, "User already exists");
+    }
+
+    console.log("req.files", req.files);
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar are required");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if (!avatar) {
+        throw new ApiError(400, "Failed to upload avatar to cloudinary");
+    }
+
+    const user = await User.create({
+        fullName,
+        email,
+        username: username.toLowerCase(),
+        password,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong creating user");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, "User created successfully", createdUser)
+    );
+
 });
 
 export { registerUser };
--- src/utils/apiResponse.js
+++ src/utils/apiResponse.js
@@ -3,10 +3,12 @@ class ApiResponse {
         statusCode,
         message = "Success",
         data
-    ){
+    ) {
         this.statusCode = statusCode;
         this.message = message;
         this.data = data;
         this.success = statusCode < 400
     }
 }
+
+export { ApiResponse };
